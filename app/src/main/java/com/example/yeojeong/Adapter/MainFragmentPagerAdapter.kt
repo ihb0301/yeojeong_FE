2452e32a8affcc95225a2c5815dc3de9290ec607
ihb0301 <abin0301@naver.com>
2025-01-23 17:59:28 +0900

    feat: ViewPager2 추가 및 프래그먼트,내비게이션바와 연결

---
+++ app/src/main/java/com/example/yeojeong/Adapter/MainFragmentPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.yeojeong.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.yeojeong.Fragment.DiaryFragment
+import com.example.yeojeong.Fragment.ExploreFragment
+import com.example.yeojeong.Fragment.HomeFragment
+import com.example.yeojeong.Fragment.PlanFragment
+import com.example.yeojeong.Fragment.ProfileFragment
+
+class MainFragmentPagerAdapter(activity : FragmentActivity) : FragmentStateAdapter(activity){
+    val fragment: List<Fragment>
+    init{
+        fragment=listOf(
+            HomeFragment(), DiaryFragment(), PlanFragment(),
+            ExploreFragment(), ProfileFragment()
+        )
+    }
+
+    override fun getItemCount(): Int=fragment.size
+
+    override fun createFragment(position: Int): Fragment=fragment[position]
+}
--- app/src/main/java/com/example/yeojeong/MainActivity.kt
+++ app/src/main/java/com/example/yeojeong/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.yeojeong.Adapter.MainFragmentPagerAdapter
 import com.example.yeojeong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,5 +26,44 @@ class MainActivity : AppCompatActivity() {
 
         //BottomNavigation 아이콘 틴트 적용 해제
         binding.mainBottomNavigation.itemIconTintList=null
+
+        //ViewPager2 Adapter 연동
+        binding.mainViewpager.adapter= MainFragmentPagerAdapter(this)
+
+        //ViewPager2와 BottomNavigation 연동
+        binding.mainBottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.page_home -> {
+                    binding.mainViewpager.currentItem = 0
+                    true
+                }
+                R.id.page_diary -> {
+                    binding.mainViewpager.currentItem = 1
+                    true
+                }
+                R.id.page_plan -> {
+                    binding.mainViewpager.currentItem = 2
+                    true
+                }
+                R.id.page_explore -> {
+                    binding.mainViewpager.currentItem = 3
+                    true
+                }
+                R.id.page_profile -> {
+                    binding.mainViewpager.currentItem = 4
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+        binding.mainViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.mainBottomNavigation.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
