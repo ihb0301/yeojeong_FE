f4835e6304ebc7455a7fb3a306ed448bda771fc6
ihb0301 <abin0301@naver.com>
2025-01-24 22:42:49 +0900

    feat: 홈 프래그먼트 툴바 추가 및 뷰 바인딩 적용

--- app/src/main/java/com/example/yeojeong/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/yeojeong/Fragment/HomeFragment.kt
@@ -2,10 +2,14 @@ package com.example.yeojeong.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.yeojeong.R
+import com.example.yeojeong.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -21,7 +25,17 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = FragmentHomeBinding.inflate(inflater)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.homeToolbar)
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_toolbar_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
