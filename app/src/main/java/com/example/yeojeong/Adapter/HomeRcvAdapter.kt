0842e1842829bc8f6bbefc8f7845480d09f9fa59
ihb0301 <abin0301@naver.com>
2025-02-04 20:08:34 +0900

    feat: 홈 화면 24시간 일기, 많이 조회된 일기 기능 구현

---
+++ app/src/main/java/com/example/yeojeong/Adapter/HomeRcvAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.yeojeong.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yeojeong.databinding.RcvHomePopularLayoutBinding
+import com.example.yeojeong.databinding.RcvHomeRecentLayoutBinding
+
+class HomeRcvPopularViewHolder(val binding: RcvHomePopularLayoutBinding): RecyclerView.ViewHolder(binding.root)
+class HomeRcvRecentViewHolder(val binding: RcvHomeRecentLayoutBinding): RecyclerView.ViewHolder(binding.root)
+
+//TODO 백엔드 구현 완료되면 String 대신 별도 객체 사용
+class HomeRcvPopularAdapter(private val popularDiaryList: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        HomeRcvPopularViewHolder(RcvHomePopularLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+    override fun getItemCount(): Int = popularDiaryList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as HomeRcvPopularViewHolder).binding
+    }
+}
+
+class HomeRcvRecentAdapter(private val recentDiaryList: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        HomeRcvRecentViewHolder(RcvHomeRecentLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as HomeRcvRecentViewHolder).binding
+
+        binding.rcvHomeRecentLayoutName.setText(recentDiaryList.get(position))
+    }
+
+    override fun getItemCount(): Int = recentDiaryList.size
+}
--- app/src/main/java/com/example/yeojeong/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/yeojeong/Fragment/HomeFragment.kt
@@ -6,8 +6,12 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.yeojeong.Adapter.HomeRcvPopularAdapter
+import com.example.yeojeong.Adapter.HomeRcvRecentAdapter
 import com.example.yeojeong.R
 import com.example.yeojeong.databinding.FragmentHomeBinding
 
@@ -28,6 +32,20 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater)
         (activity as AppCompatActivity?)!!.setSupportActionBar(binding.homeToolbar)
         setHasOptionsMenu(true)
+
+        //리사이클러 뷰 임시 테스트용 리스트 2개
+        val list1= mutableListOf<String>("사람1","사람2","사람3","사람4","사람5","사람6","사람7")
+        val list2= mutableListOf<String>("본문1","본문2","본문3","본문4","본문5")
+
+        //많이 조회된 일기 리사이클러 뷰
+        binding.homePopularRecyclerView.layoutManager = LinearLayoutManager(activity)
+        binding.homePopularRecyclerView.adapter= HomeRcvPopularAdapter(list2)
+
+        //최근 일기 리사이클러 뷰
+        binding.homeRecentRecyclerView.layoutManager = LinearLayoutManager(activity)
+            .apply{ orientation=LinearLayoutManager.HORIZONTAL }
+        binding.homeRecentRecyclerView.adapter= HomeRcvRecentAdapter(list1)
+
 
         // Inflate the layout for this fragment
         return binding.root
