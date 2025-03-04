b8dfde0107b147f9446abcbab7f3c892541291e6
ihb0301 <abin0301@naver.com>
2025-03-04 22:54:41 +0900

    design: 마이페이지 디자인 구현

--- app/src/main/java/com/example/yeojeong/Fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/yeojeong/Fragment/MyPageFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.yeojeong.Adapter.HomeRcvPopularAdapter
 import com.example.yeojeong.R
+import com.example.yeojeong.databinding.FragmentMypageBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -22,7 +25,11 @@ class MyPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        val binding=FragmentMypageBinding.inflate(inflater,container,false)
+
+        binding.rcvMypage.layoutManager=LinearLayoutManager(activity)
+        binding.rcvMypage.adapter=HomeRcvPopularAdapter(mutableListOf("1","2","3","4","5"))
+
+        return binding.root
     }
 }
