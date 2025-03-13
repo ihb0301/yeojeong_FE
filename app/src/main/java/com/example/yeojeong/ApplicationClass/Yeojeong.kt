107ab7ddad3580a850d3336c09c3d77edd6574da
ihb0301 <abin0301@naver.com>
2025-03-13 17:00:07 +0900

    feat: 오류 발생 다이얼로그와 메시지 추가

--- app/src/main/java/com/example/yeojeong/ApplicationClass/Yeojeong.kt
+++ app/src/main/java/com/example/yeojeong/ApplicationClass/Yeojeong.kt
@@ -1,6 +1,14 @@
 package com.example.yeojeong.ApplicationClass
 
 import android.app.Application
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.view.WindowManager
+import android.widget.TextView
+import com.example.yeojeong.R
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,5 +21,28 @@ class Yeojeong: Application() {
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(url)
                 .build()
+
+        /*
+            다이얼로그 생성을 위한 함수
+            사용법 예시: Yeojeong.makeDialog(applicationContext ,"표시할 메시지")
+        */
+        fun makeDialog(context: Context,message: String){
+            val dialog= Dialog(context)
+            dialog.setContentView(R.layout.dialog_layout)
+            dialog.window?.apply{
+                setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
+                    WindowManager.LayoutParams.FLAG_BLUR_BEHIND)
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.S){
+                    //안드로이드 버전 12 이상에서 블러 배경 적용
+                    setBackgroundBlurRadius(80)
+                }
+            }
+            dialog.findViewById<TextView>(R.id.message_text).setText(message)
+            dialog.findViewById<TextView>(R.id.ok_button).setOnClickListener{
+                dialog.hide()
+            }
+            dialog.show()
+        }
     }
 }
